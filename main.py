61ac772d84e635d048e08227aa0ed6b9c1971214
nirojpradhan <nirojpradhan@yahoo.com>
2025-03-20 20:39:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,97 +1,141 @@
 import streamlit as st
-from langchain import PromptTemplate
 from langchain_openai import OpenAI
-from langchain.chains.summarize import load_summarize_chain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import pandas as pd
-from io import StringIO
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.evaluation.qa import QAEvalChain
 
-#LLM and key loading function
-def load_LLM(openai_api_key):
-    # Make sure your openai_api_key is set as an environment variable
-    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
-    return llm
+def generate_response(
+    uploaded_file,
+    openai_api_key,
+    query_text,
+    response_text
+):
+    #format uploaded file
+    documents = [uploaded_file.read().decode()]
+    
+    #break it in small chunks
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=0
+    )
+    texts = text_splitter.create_documents(documents)
+    embeddings = OpenAIEmbeddings(
+        openai_api_key=openai_api_key
+    )
+    
+    # create a vectorstore and store there the texts
+    db = FAISS.from_documents(texts, embeddings)
+    
+    # create a retriever interface
+    retriever = db.as_retriever()
+    
+    # create a real QA dictionary
+    real_qa = [
+        {
+            "question": query_text,
+            "answer": response_text
+        }
+    ]
+    
+    # regular QA chain
+    qachain = RetrievalQA.from_chain_type(
+        llm=OpenAI(openai_api_key=openai_api_key),
+        chain_type="stuff",
+        retriever=retriever,
+        input_key="question"
+    )
+    
+    # predictions
+    predictions = qachain.apply(real_qa)
+    
+    # create an eval chain
+    eval_chain = QAEvalChain.from_llm(
+        llm=OpenAI(openai_api_key=openai_api_key)
+    )
+    # have it grade itself
+    graded_outputs = eval_chain.evaluate(
+        real_qa,
+        predictions,
+        question_key="question",
+        prediction_key="result",
+        answer_key="answer"
+    )
+    
+    response = {
+        "predictions": predictions,
+        "graded_outputs": graded_outputs
+    }
+    
+    return response
 
+st.set_page_config(
+    page_title="Evaluate a RAG App"
+)
+st.title("Evaluate a RAG App")
 
-#Page title and header
-st.set_page_config(page_title="AI Long Text Summarizer")
-st.header("AI Long Text Summarizer")
+with st.expander("Evaluate the quality of a RAG APP"):
+    st.write("""
+        To evaluate the quality of a RAG app, we will
+        ask it questions for which we already know the
+        real answers.
+        
+        That way we can see if the app is producing
+        the right answers or if it is hallucinating.
+    """)
 
+uploaded_file = st.file_uploader(
+    "Upload a .txt document",
+    type="txt"
+)
 
-#Intro: instructions
-col1, col2 = st.columns(2)
+query_text = st.text_input(
+    "Enter a question you have already fact-checked:",
+    placeholder="Write your question here",
+    disabled=not uploaded_file
+)
 
-with col1:
-    st.markdown("ChatGPT cannot summarize long texts. Now you can do it with this app.")
+response_text = st.text_input(
+    "Enter the real answer to the question:",
+    placeholder="Write the confirmed answer here",
+    disabled=not uploaded_file
+)
 
-with col2:
-    st.write("Contact with [AI Accelera](https://aiaccelera.com) to build your AI Projects")
+result = []
+with st.form(
+    "myform",
+    clear_on_submit=True
+):
+    openai_api_key = st.text_input(
+        "OpenAI API Key:",
+        type="password",
+        disabled=not (uploaded_file and query_text)
+    )
+    submitted = st.form_submit_button(
+        "Submit",
+        disabled=not (uploaded_file and query_text)
+    )
+    if submitted and openai_api_key.startswith("sk-"):
+        with st.spinner(
+            "Wait, please. I am working on it..."
+            ):
+            response = generate_response(
+                uploaded_file,
+                openai_api_key,
+                query_text,
+                response_text
+            )
+            result.append(response)
+            del openai_api_key
+            
+if len(result):
+    st.write("Question")
+    st.info(response["predictions"][0]["question"])
+    st.write("Real answer")
+    st.info(response["predictions"][0]["answer"])
+    st.write("Answer provided by the AI App")
+    st.info(response["predictions"][0]["result"])
+    st.write("Therefore, the AI App answer was")
+    st.info(response["graded_outputs"][0]["results"])
 
-
-#Input OpenAI API Key
-st.markdown("## Enter Your OpenAI API Key")
-
-def get_openai_api_key():
-    input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input", type="password")
-    return input_text
-
-openai_api_key = get_openai_api_key()
-
-
-# Input
-st.markdown("## Upload the text file you want to summarize")
-
-uploaded_file = st.file_uploader("Choose a file", type="txt")
-
-       
-# Output
-st.markdown("### Here is your Summary:")
-
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    #st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    #st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    #st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    #dataframe = pd.read_csv(uploaded_file)
-    #st.write(dataframe)
-
-    file_input = string_data
-
-    if len(file_input.split(" ")) > 20000:
-        st.write("Please enter a shorter file. The maximum length is 20000 words.")
-        st.stop()
-
-    if file_input:
-        if not openai_api_key:
-            st.warning('Please insert OpenAI API Key. \
-            Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', 
-            icon="⚠️")
-            st.stop()
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n"], 
-        chunk_size=5000, 
-        chunk_overlap=350
-        )
-
-    splitted_documents = text_splitter.create_documents([file_input])
-
-    llm = load_LLM(openai_api_key=openai_api_key)
-
-    summarize_chain = load_summarize_chain(
-        llm=llm, 
-        chain_type="map_reduce"
-        )
-
-    summary_output = summarize_chain.run(splitted_documents)
-
-    st.write(summary_output)
